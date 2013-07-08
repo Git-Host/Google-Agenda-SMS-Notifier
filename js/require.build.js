4e395f1215b2f528eb280547d009678d70f0a134
mpeg <mpeg@blackbeardcrew.com>
2013-07-08 12:33:53 +0200

    optimizer script

--- js/require.build.js
+++ js/require.build.js
@@ -3,22 +3,40 @@
  * Bricks for Mobile App Directors
  * ===============================
  * 
- * RequireJs configuration
+ * RequireJs Optimizer Configuration
+ * 
+ * It creates an "/AppDeploy" directory under your App's project folder.
+ * After that you can remove "/AppDeploy/js/" and "/AppDeploy/less/" folders to reduce
+ * production folder size.
  *
+ * Please be carefull to remove hidden ".git" folder who can be quite heavy!
  */
 
-
-
-
 ({
-	appDir: 	'../',
-	baseUrl: 	'./',
+	
+	
+	/**
+	 * PRODUCTION FOLDER PATH
+	 * Here you can configure where to save production files
+	 */
 	dir: 		'../AppDeploy/',
 	
-	optimize: "none",
 	
+	/**
+	 * UGLIFICATION & COMPRESSION
+	 * optimized files are uglified and compressed by default.
+	 * uncomment this instruction to skip uglification and obtain
+	 * a single source file who still be debuggable!
+	 */
+	//optimize: "none",
+	
+	
+	
+	
+	appDir: 	'../',
+	baseUrl: 	'./',
 	mainConfigFile: "../require.app.js",
-	
 	modules:	[{name:'require.app'}]
 	
 })
+
